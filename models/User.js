026f6dbe81dffd78fe86ee62058449a97909c5ca
sitobusnan <sito@citytonight.es>
2018-11-27 12:37:53 +0100

    drop collection added

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  id: String,
+  id: {type: String, unique: true},
   team_id: String,
   email: String,
   first_name: String,
--- seeds.js
+++ seeds.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Seeds file that remove all users and create 2 new users
 
@@ -12,41 +12,67 @@ const axios = require("axios");
 const bcryptSalt = 11;
 
 mongoose
-  .connect('mongodb://localhost/iron-tinder', {useNewUrlParser: true})
+  .connect(
+    "mongodb://localhost/iron-tinder",
+    { useNewUrlParser: true }
+  )
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+    User.collection.drop()
+  .then(() => {
+
+    axios
+    .get(
+      `https://slack.com/api/channels.info?token=${process.env.TOKEN}&channel=${
+        process.env.GROUP
+      }&pretty=1`
+    )
+    .then(response => {
+      console.log(response.data.channel.members);
+      response.data.channel.members.forEach(element => {
+        axios
+          .get(
+            `https://slack.com/api/users.info?token=${
+              process.env.TOKEN
+            }&user=${element}&pretty=1`
+          )
+  
+          .then(singleuser => {
+            const userData = singleuser.data.user;
+            const userProfile = singleuser.data.user.profile;
+            const newUser = new User();
+            newUser.id = userData.id;
+            newUser.team_id = userData.team_id;
+            newUser.email = userProfile.email;
+            newUser.first_name = userProfile.first_name;
+            newUser.last_name = userProfile.last_name;
+            newUser.display_name = userProfile.display_name;
+            newUser.avatar_img = `https://ca.slack-edge.com/${userData.team_id}-${
+              userData.id
+            }-${userProfile.avatar_hash}-1024`;
+  
+            newUser
+              .save()
+              .then(() => {
+                console.log("User created");
+              })
+              .catch(error => {
+                console.log("Error to add a new user" + error);
+              });
+          });
+      });
+    });
+  
+
+
+   })
+
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-  axios.get(`https://slack.com/api/channels.info?token=${process.env.TOKEN}&channel=${process.env.GROUP}&pretty=1`)
- .then((response) => {
-  console.log(response.data.channel.members)
-   response.data.channel.members.forEach(element => {
-    axios.get(`https://slack.com/api/users.info?token=${process.env.TOKEN}&user=${element}&pretty=1`)
-    .then((singleuser)=> {
-      const userData = singleuser.data.user
-      const userProfile = singleuser.data.user.profile
-      const newUser = new User();
-      newUser.id = userData.id;
-      newUser.team_id = userData.team_id;
-      newUser.email = userProfile.email;
-      newUser.first_name = userProfile.first_name;
-      newUser.last_name = userProfile.last_name;
-      newUser.display_name = userProfile.display_name;
-      newUser.avatar_img = `https://ca.slack-edge.com/${userData.team_id}-${userData.id}-${userProfile.avatar_hash}-1024`;
-      newUser.save()
-      .then(() => {
-        console.log("User created")
-      })
-      .catch(error => {
-        console.log("Error to add a new user" + error);
-        
-      });
-    })
-   });
- });
 
-
-
+  
